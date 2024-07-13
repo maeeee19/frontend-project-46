9ae453f9b81a9cd2d2918a34208ddd8e4fb77700
Dmitry Maslanov <maslanovdimasik@gmail.com>
2024-07-13 11:56:49 +0300

    шаг1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/maeeee19/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/maeeee19/frontend-project-46/actions)
+<a href="https://codeclimate.com/github/maeeee19/frontend-project-46/maintainability"><img src="https://api.codeclimate.com/v1/badges/ffcc787f0f5f027980f0/maintainability" /></a>
---
+++ bin/gendiff.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import process from 'process'; 
+
+program
+  .version('1.0.0') 
+  .option('-v', 'output the version number')
+  .option('-h', 'output usage information') 
+  .parse(process.argv);
+
+if (program.help) {
+  console.log('  Usage: gendiff [options] [filepath1] [filepath2]');
+  console.log('  Compares two configuration files and shows a difference.');
+  console.log('');
+  console.log('  Options:');
+  console.log('    -V, --version        output the version number');
+  console.log('    -h, --help           output usage information');
+}
