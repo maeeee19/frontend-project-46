d0725bd61f2dc98c9169934d8cbafd22ef37bfe4
Dmitry Maslanov <maslanovdimasik@gmail.com>
2024-07-14 15:11:06 +0300

    шаг2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,10 +10,11 @@ program
   .parse(process.argv);
 
 if (program.help) {
-  console.log('  Usage: gendiff [options] [filepath1] [filepath2]');
+  console.log('  Usage: gendiff [options] <filepath1> <filepath2>');
   console.log('  Compares two configuration files and shows a difference.');
   console.log('');
   console.log('  Options:');
   console.log('    -V, --version        output the version number');
+  console.log('    -f, --format [type]  output format');
   console.log('    -h, --help           output usage information');
 }
