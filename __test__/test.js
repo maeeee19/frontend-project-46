2fd9eb988deb985b3f9325a259f91325938d3ac5
Dmitry Maslanov <maslanovdimasik@gmail.com>
2025-06-19 22:32:54 +0300

    правки

--- __test__/test.js
+++ __test__/test.js
@@ -8,52 +8,27 @@ const getFixturePath = (filename, dir) => {
   const __filename = fileURLToPath(import.meta.url);
   const dirname = path.dirname(__filename);
 
-<<<<<<< HEAD
- return path.join(dirname, '..', dir ? `__fixtures__/${dir}` : '__fixtures__', filename);
+  return path.join(dirname, '..', dir ? `__fixtures__/${dir}` : '__fixtures__', filename);
 };
 
 const file1Data = ['file1.json', 'file1.yaml', 'file1.yml']
 const file2Data = ['file2.json', 'file2.yaml', 'file2.yml']
 const formats = ['stylish', 'plain', 'json']
-=======
-  return path.join(dirname, '..', dir ? `__fixtures__/${dir}` : '__fixtures__', filename);
-};
-
-const file1Data = ['file1.json', 'file1.yaml', 'file1.yml'];
-const file2Data = ['file2.json', 'file2.yaml', 'file2.yml'];
-const formats = ['stylish', 'plain', 'json'];
-const types = ['flat', 'deep'];
->>>>>>> 7ec56c45578ae1adb895f16d1ff9463ff5183f77
 
 const expectTest = (file1, file2, format) => {
   expect(
-<<<<<<< HEAD
       genDiff(getFixturePath(file1), getFixturePath(file2), format),
     ).toEqual(fs.readFileSync(getFixturePath(`${format}-result.txt`), 'utf-8'));
 }
-=======
-    genDiff(getFixturePath(file1, type), getFixturePath(file2, type), format),
-  ).toEqual(fs.readFileSync(getFixturePath(`${format}-${type}-result.txt`), 'utf-8'));
-};
->>>>>>> 7ec56c45578ae1adb895f16d1ff9463ff5183f77
+
 
 file1Data.forEach((file1) => {
   file2Data.forEach((file2) => {
     formats.forEach((format) => {
-<<<<<<< HEAD
       test(`equal ${file1} with ${file2} in ${format} format`, () => {
         expectTest(file1, file2, format);
       });
     });
   });
 });
-=======
-      types.forEach((type) => {
-        test(`${type} equal ${file1} with ${file2} in ${format} format`, () => {
-          expectTest(file1, file2, format, type);
-        });
-      });
-    });
-  });
-});
->>>>>>> 7ec56c45578ae1adb895f16d1ff9463ff5183f77
+
--- src/formatters/JSON.js
+++ src/formatters/JSON.js
@@ -1,5 +1,4 @@
 const jsonFormatter = (diffTree) => {
-<<<<<<< HEAD
   const convertValue = (value) => {
     if (typeof value === 'object' && value !== null) {
       return JSON.parse(JSON.stringify(value));
@@ -10,51 +9,30 @@ const jsonFormatter = (diffTree) => {
   const convertNode = (node) => {
     if (node.type === 'nested') {
       return {
-        ...node.children.reduce((acc, child) => ({
-          ...acc,
-          [child.key]: convertNode(child),
-        }), {}),
-=======
-  const convertNode = (node) => {
-    if (node.type === 'nested') {
-      return {
         ...node.children.reduce((acc, child) => (
           {
             ...acc,
             [child.key]: convertNode(child),
           }), {}),
->>>>>>> 7ec56c45578ae1adb895f16d1ff9463ff5183f77
       };
     }
 
     if (node.type === 'changed') {
       return {
         changing: node.type,
-<<<<<<< HEAD
         value1: convertValue(node.oldValue),
         value2: convertValue(node.newValue),
-=======
-        value1: node.oldValue,
-        value2: node.newValue,
->>>>>>> 7ec56c45578ae1adb895f16d1ff9463ff5183f77
       };
     }
 
     return { changing: node.type, value: convertValue(node.value) };
   };
 
-<<<<<<< HEAD
-  return JSON.stringify(diffTree.reduce((acc, node) => ({
-    ...acc,
-    [node.key]: convertNode(node),
-  }), {}));
-=======
   return JSON.stringify(diffTree.reduce((acc, node) => (
     {
       ...acc,
       [node.key]: convertNode(node),
     }), {}));
->>>>>>> 7ec56c45578ae1adb895f16d1ff9463ff5183f77
 };
 
 export default jsonFormatter;
